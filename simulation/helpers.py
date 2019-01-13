27e5b8571dffeba9491072c7de16fe0de4381c67
liamzebedee <liamzebedee@yahoo.com.au>
2019-01-13 17:56:40 +0100

    Refactor node id generator

--- simulation/helpers.py
+++ simulation/helpers.py
@@ -1,6 +1,17 @@
+import itertools
+
 def id_generator():
     id = 0
     while(True):
         yield str(id)
         id += 1
 
+class NodeIdGenerator():
+	def __init__(self):
+		self.gen = id_generator()
+	
+	def __call__(self, amt):
+		if amt == 1:
+			return next(self.gen)
+		else:
+			return list(itertools.islice(self.gen, 10))
