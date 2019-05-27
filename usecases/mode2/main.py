c240725977ed649cde703b5d9d0ad4e135b58c50
liamzebedee <liamzebedee@yahoo.com.au>
2019-05-27 17:47:53 +0200

    Test GPG signing of commits

---
+++ usecases/mode2/main.py
@@ -0,0 +1,52 @@
+# Organisation
+
+# Members
+# Invest a portion each
+
+# They undergo activities
+# Where they generate value for the organisation
+
+
+# To have subjective consensus WITH uncertainty
+# You can have subjective consensus without, that is just an oracle
+
+
+class Organistion():
+    def __init__(self, members):
+        self.members = []
+    
+    def add_member(self, member):
+        pass
+        # self.members.append(member)
+    
+    def vote(self, head):
+        pass
+    
+    def tally(self):
+        # Get the consensus of the opinion on this matter
+        # Calculate sharepower by PageRank
+
+
+
+
+for member in members:
+    org.change_stake(other_member, .95, 0., 0.05).send(member)
+
+
+# Now we submit a proposal
+# for example, whether x is a human
+
+proposal_id = org.propose(x).send(member)
+
+
+# And vote
+
+for member in members:
+    org.vote(proposal_id, 1.0, 0., 0.).send(member)
+
+
+# Organisation now needs to tally votes
+
+org.tally()
+
+# And decide to allocate funds
