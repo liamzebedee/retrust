68db980560609eab5d7ecabebceb8c3cd08cfaa6
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-15 20:16:56 +0200

    Fix up not tracking user registration

--- contracts/deploy/deploy.sh
+++ contracts/deploy/deploy.sh
@@ -20,13 +20,17 @@ seth send --create $(cat ./vyper/ebsl.bytecode)
 
 seth send --create $(cat ./out/TrustToken.bin)
 
-export EBSL_CORE=0x2ac80994c6ea3dc546d5ad35aba1d60bb66290ce
-export TRUST_TOKEN=0x8de58e2b82e23079a83a6af60e41b8727ed3f34b
+export EBSL_CORE=0x243e72b69141f6af525a9a5fd939668ee9f2b354
+export TRUST_TOKEN=0x2a212f50a2a020010ea88cc33fc4c333e6a5c5c4
 
 
 seth send --create $(cat ./out/EBSL.bin) "EBSLCore(address,address)" $EBSL_CORE $TRUST_TOKEN
-export EBSL=0x1153f042bc629e1929e7e085c71f8ee0db28b0bb
+export EBSL=0xd0828aeb00e4db6813e2f330318ef94d2bba2f60
 
 
 seth call $EBSL_CORE "check_xtol(uint168,uint168)(bool)" 1012329203 123981231782
 seth call $EBSL_CORE "to_opinion(int128)(uint168[3] memory)" 3
+
+seth call $EBSL "ebslConverge2()"
+
+seth send $TRUST_TOKEN "trust(address,address,int128)" $ETH_FROM $ETH_FROM 100
--- contracts/src/TrustToken.sol
+++ contracts/src/TrustToken.sol
@@ -1,18 +1,28 @@
 
 
 contract TrustToken {
-    uint256 userCount;
+    uint256 userCount = 0;
     mapping(uint    => address) usersReverse;
     mapping(address => uint) users;
 
     mapping(address => mapping(address => int128)) balances;
     
 
+    function registered(address user) internal {
+        if(users[user] == 0x0) {
+            usersReverse[userCount] = user;
+            users[user] = userCount;
+            userCount += 1;
+        }
+    }
+
     function trust(address from, address to, int128 amt) public {
+        registered(from); registered(to);
         balances[from][to] += amt;
     }
 
     function distrust(address from, address to, int128 amt) public {
+        registered(from); registered(to);
         balances[from][to] -= amt;
     }
 
