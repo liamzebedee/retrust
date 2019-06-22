b1caa046a2c8499cd940065c2d58d4f878426e99
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-22 02:57:36 +0200

    nice deploy script

--- contracts/deploy/deploy.sh
+++ contracts/deploy/deploy.sh
@@ -16,21 +16,15 @@ export ETH_FROM=$(seth accounts | head -n1 | awk '{ print $1 }')
 
 
 
-seth send --create $(cat ./vyper/ebsl.bytecode)
-
-seth send --create $(cat ./out/TrustToken.bin)
-
-export EBSL_CORE=0x243e72b69141f6af525a9a5fd939668ee9f2b354
-export TRUST_TOKEN=0x2a212f50a2a020010ea88cc33fc4c333e6a5c5c4
+export EBSL_CORE=$(seth send --create $(cat ../vyper/ebsl.bytecode) --status 2>/dev/null)
+echo -n $EBSL_CORE > EBSLCore
 
 
-seth send --create $(cat ./out/EBSL.bin) "EBSLCore(address,address)" $EBSL_CORE $TRUST_TOKEN
-export EBSL=0xd0828aeb00e4db6813e2f330318ef94d2bba2f60
+
+export TRUST_TOKEN=$(seth send --create $(cat ../out/TrustToken.bin) --status 2>/dev/null)
+echo -n $TRUST_TOKEN > TrustToken
 
 
-seth call $EBSL_CORE "check_xtol(uint168,uint168)(bool)" 1012329203 123981231782
-seth call $EBSL_CORE "to_opinion(int128)(uint168[3] memory)" 3
 
-seth call $EBSL "ebslConverge2()"
-
-seth send $TRUST_TOKEN "trust(address,address,int128)" $ETH_FROM $ETH_FROM 100
+export EBSL=$(seth send --create $(cat ../out/EBSL.bin) "EBSLCore(address,address)" $EBSL_CORE $TRUST_TOKEN --status 2>/dev/null)
+echo -n $EBSL > EBSL
